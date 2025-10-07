dac681c030de66832e552e11470e758000771770
Александр <232641072+Alehsad@users.noreply.github.com>
2025-10-07 23:08:48 +0300

    2

---
+++ src/lab02/matrix.py/ex03.py
@@ -0,0 +1,13 @@
+def col_sums(matrix):
+
+    length = len(matrix[0])
+    for row in matrix:
+        if len(row) != length:
+            return "ValueError"
+    
+    return [sum(col) for col in zip(*matrix)]
+
+print(col_sums([[1, 2, 3], [4, 5, 6]]))
+print(col_sums([[-1, 1], [10, -10]]))
+print(col_sums([[0, 0], [0, 0]]))
+print(col_sums([[1, 2], [3]]))
